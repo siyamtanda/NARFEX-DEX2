daa118337f620a1d9664a296554a9e93bd47fc7c
siyamtanda <68695019+siyamtanda@users.noreply.github.com>
2022-04-07 15:11:47 +0200

    Update Narfex-Dex.sol
    Narfex DEX with Narfex Utility Token.

--- contracts/Narfex-Dex.sol
+++ contracts/Narfex-Dex.sol
@@ -28,7 +28,7 @@ library Counters {
     function current(Counter storage counter) internal view returns (uint256) {
         return counter._value;
     }
-
+    
     function increment(Counter storage counter) internal {
         unchecked {
             counter._value += 1;
